43b7dc3b1bce511303dcd59cb4b8ecf7d32a4349
Dona <donamattel@gmail.com>
2023-06-20 21:14:03 +0530

    route change

--- clientnx/api.js
+++ clientnx/api.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 const api =axios.create({
-    baseURL:"http://localhost:8080"
+    //baseURL:"http://localhost:8080"
+    baseURL:'https://homecareback.onrender.com'
 })
 export default api
