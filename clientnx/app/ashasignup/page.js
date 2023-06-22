f67581de229bd0e364a47b91d278c1360f3df2e6
Amalia <amaliasaleena19@gmail.com>
2023-06-22 22:09:12 +0530

    Frontend for Asha sign up page

--- clientnx/app/ashasignup/page.js
+++ clientnx/app/ashasignup/page.js
@@ -1,17 +1,26 @@
+"use client";
+import { useRouter } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import { Button, Checkbox, Form, Input } from "antd";
 
-'use client'
-import { useRouter } from 'next/navigation';
-import React, { useState, useEffect } from 'react';
+const onFinish = (values) => {
+  console.log("Success:", values);
+};
+const onFinishFailed = (errorInfo) => {
+  console.log("Failed:", errorInfo);
+};
 
 const Signup = () => {
-  const [ashaId, setAshaId] = useState('');
-  const [email, setEmail] = useState('');
-const router = useRouter()
+  const [ashaId, setAshaId] = useState("");
+  const [email, setEmail] = useState("");
+  const router = useRouter();
   const handleSubmit = (e) => {
-    const isEmailValid = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email);
+    const isEmailValid = /^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(
+      email
+    );
 
-     if (!isEmailValid) {
-      alert('Please enter a valid email address.');
+    if (!isEmailValid) {
+      alert("Please enter a valid email address.");
     }
     e.preventDefault();
 
@@ -19,37 +28,91 @@ const router = useRouter()
       return;
     }
 
-    router.push('/ashalogin');
+    router.push("/ashalogin");
   };
 
   useEffect(() => {
     if (!ashaId.trim() || !email.trim()) {
       return;
     }
-
-    
-    
   }, [ashaId, email]);
 
   return (
-    <div>
-      <h1>Sign Up</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="ASHA ID"
-          value={ashaId}
-          onChange={(e) => setAshaId(e.target.value)}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <button type="submit">Sign Up</button>
-      </form>
-    </div>
+    <section>
+      <div className="circle"></div>
+      <div className="container">
+        <div className="rounded-rectangle-login">
+          <h2 className="heading3">Sign Up</h2>
+          <Form
+            onSubmit={handleSubmit}
+            name="basic"
+            labelCol={{
+              span: 8,
+            }}
+            wrapperCol={{
+              span: 16,
+            }}
+            style={{
+              maxWidth: 600,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Asha ID"
+              name="ashaId"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Asha ID!",
+                },
+              ]}
+            >
+              <Input onChange={(e) => setAshaId(e.target.value)} />
+            </Form.Item>
+
+            <Form.Item
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                },
+              ]}
+            >
+              <Input onChange={(e) => setEmail(e.target.value)} />
+            </Form.Item>
+
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+
+            <Form.Item
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
+    </section>
   );
 };
 
