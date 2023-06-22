3f05df08bc51dc5478596cd8d653c01f10bd81ad
Amalia <amaliasaleena19@gmail.com>
2023-06-22 21:46:16 +0530

    Frontend for Admin login and admin homepage

--- clientnx/app/adminlogin/page.js
+++ clientnx/app/adminlogin/page.js
@@ -1,114 +1,130 @@
-'use client'
+"use client";
 
-import api from '@/api'
+import api from "@/api";
 
-import { Form, Input, message ,Checkbox, Button} from 'antd'
-import { useRouter } from 'next/navigation'
-import React from 'react'
-
+import { Form, Input, message, Checkbox, Button } from "antd";
+import { useRouter } from "next/navigation";
+import React from "react";
 
 const Login = () => {
-  const router= useRouter()
-  
+  const router = useRouter();
+
   //on finish handler function
-  const onFinishHandler = async(values)=>{
+  const onFinishHandler = async (values) => {
     try {
       //dispatch(showloading())
-      const res = await api.post('/api/v1/user/login',values)
-    //dispatch(hideLoading())
-      if(res.data.success && res.data.isAdmin){
-    localStorage.setItem("token",res.data.token)
-    message.success("Login Successful")
-    router.push('/adminhome')
-   }
-   else if(res.data.success && !(res.data.isAdmin))
-   {
-    message.error("YOu are not admin")
-    router.push('/')
-   }
-   else{
-    message.error("something went wrong")
-    message.error(res.data.message)
-   }
-      
+      const res = await api.post("/api/v1/user/login", values);
+      //dispatch(hideLoading())
+      if (res.data.success && res.data.isAdmin) {
+        localStorage.setItem("token", res.data.token);
+        message.success("Login Successful");
+        router.push("/adminhome");
+      } else if (res.data.success && !res.data.isAdmin) {
+        message.error("YOu are not admin");
+        router.push("/");
+      } else {
+        message.error("something went wrong");
+        message.error(res.data.message);
+      }
     } catch (error) {
-     // dispatch(hideloading())
-      console.log(error)
-      message.error("something went wrong")
-      
+      // dispatch(hideloading())
+      console.log(error);
+      message.error("something went wrong");
     }
-  }
+  };
   return (
-    <div className='adminlogin-form'><h1 align ="center" className='Adminlogin' font-color='white'> Admin login</h1>
-    <Form
-    name="basic"
-    labelCol={{
-      span: 8,
-    }}
-    wrapperCol={{
-      span: 16,
-    }}
-    style={{
-      maxWidth: 600,
-    }}
-    initialValues={{
-      remember: true,
-    }}
-    onFinish={onFinishHandler}
-    className='adminloginbox'
-   
-  >
-    <Form.Item
-      label="Username"
-      name="email"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your username!',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
+    <section>
+      <div className="circle"></div>
+      <div className="container-short">
+        <h2 className="heading5">Hello!</h2>
+      </div>
+      <div className="container-short">
+        <div className="rounded-rectangle-login">
+          <h2 className="heading4">Login</h2>
 
-    <Form.Item
-      label="Password"
-      name="password"
-      rules={[
-        {
-          required: true,
-          message: 'Please input your password!',
-        },
-      ]}
-    >
-      <Input.Password />
-    </Form.Item>
+          <div className="container">
+            <div>
+              <Form
+                className="form-container"
+                name="basic"
+                labelCol={{
+                  span: 8,
+                }}
+                wrapperCol={{
+                  span: 16,
+                }}
+                style={{
+                  maxWidth: 600,
+                  maxHeight: 600,
+                }}
+                initialValues={{
+                  remember: true,
+                }}
+                onFinish={onFinishHandler}
+                autoComplete="off"
+              >
+                <Form.Item
+                  className="form-label"
+                  label="Username"
+                  name="username"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your username!",
+                    },
+                  ]}
+                  style={{ color: "#ffffff" }}
+                >
+                  <Input className="form-input" />
+                </Form.Item>
 
-    <Form.Item
-      name="remember"
-      valuePropName="checked"
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      <Checkbox>Remember me</Checkbox>
-    </Form.Item>
+                <Form.Item
+                  className="form-label"
+                  label="Password"
+                  name="password"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password!",
+                    },
+                  ]}
+                >
+                  <Input.Password className="form-input" />
+                </Form.Item>
 
-    <Form.Item
-      wrapperCol={{
-        offset: 8,
-        span: 16,
-      }}
-    >
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-    
-    </div>
-  )
-}
+                <Form.Item
+                  className="form-label form-item-remember"
+                  name="remember"
+                  valuePropName="checked"
+                  wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                  }}
+                >
+                  <Checkbox>Remember me</Checkbox>
+                </Form.Item>
 
-export default Login
+                <Form.Item
+                  wrapperCol={{
+                    offset: 8,
+                    span: 16,
+                  }}
+                >
+                  <Button type="primary" htmlType="submit">
+                    Submit
+                  </Button>
+                </Form.Item>
+              </Form>
+            </div>
+
+            <div></div>
+
+            <div></div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
--- clientnx/app/page.js
+++ clientnx/app/page.js
@@ -1,16 +1,35 @@
+"use client";
+import { Button } from "antd";
 
-import { Button } from 'antd'
-
-import Link from 'next/link'
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  
+  const router = useRouter();
+  const OnclickNav = () => {
+    router.push("/adminlogin");
+  };
+  const OnclickNavS = () => {
+    router.push("/signup");
+  };
   return (
-    <div>
-      <h1>Homepage</h1>
-     <Link href="/adminlogin">Login</Link> <br>
-     </br>
-     <Link href="/adminlogin">signup</Link>
-    </div>
-  )
+    <section>
+      <div className="circle"> </div>
+      <h1 className="heading2"> HomeCare </h1>
+      <p className="center">Meenadam Palliative Care</p>
+      <div className="container">
+        <div className="rounded-rectangle-button">
+          <Button onClick={OnclickNavS} className="inside-button1">
+            Sign Up
+          </Button>
+        </div>
+
+        <div className="rounded-rectangle-button">
+          <Button onClick={OnclickNav} className="inside-button1">
+            Login
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
 }
