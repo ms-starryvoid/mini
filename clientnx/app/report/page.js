ae92fc9273bd0b98a663a91037f3661a07a9bd56
Edna Jason <ednajason28@gmail.com>
2023-06-27 01:58:36 +0530

    updated report page

--- clientnx/app/report/page.js
+++ clientnx/app/report/page.js
@@ -5,30 +5,43 @@ import api from "@/api";
 import { Form, Input, message, Checkbox, Button } from "antd";
 import { useRouter } from "next/navigation";
 import React from "react";
+import Layout from "../layout";
 
 
-const Page3 = () => {
-    return (
-      <div>
-        <div>
-          <label>Blood Sugar:</label>
-          <br />
-          <input type="text" />
-        </div>
-  
-        <div>
-          <label>Blood Pressure:</label>
-          <br />
-          <input type="text" />
-        </div>
-  
-        <div>
-          <label>Remarks:</label>
-          <br />
-          <textarea rows="4" />
-        </div>
+const Report = () => {
+    return(
+        <Layout>
+      <div className="asd">
+    <Form
+        //   labelCol={{
+        //     span: 10,
+        //   }}
+        //   wrapperCol={{
+        //     span: 20,
+        //   }}
+          layout="horizontal"
+        //   disabled={componentDisabled}
+        //   style={{
+        //     maxWidth: 1000,
+        //   }}
+        //   onFinish={onFinish}
+          >
+
+          <p>
+                  <Form.Item label="Blood Sugar" name="blood_sugar">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item label="Blood Pressure" name="blood_pressure">
+                    <Input />
+                  </Form.Item>
+                  <Form.Item label="Remarks" name="remarks">
+                    <Input />
+                  </Form.Item>
+          </p>       
+          </Form>
       </div>
-    );
-  };
+    </Layout>
+    )
+        }
 
-  export default Page3
+  export default Report
