6f07db39e4f82b353f27f06b2ed5cb1d60ac138c
Edna Jason <ednajason28@gmail.com>
2023-08-06 23:11:25 +0530

    reportadmin

---
+++ clientnx/app/adminprofile/[slug]/page.js
@@ -0,0 +1,102 @@
+"use client";
+
+import api from "@/api";
+
+import { Form, Input, message, Checkbox, Button } from "antd";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+
+const AdminProfile = () => {
+  const [userdetails, setuserdetails] = useState([]);
+  const [editing, setEditing] = useState(false);
+
+  const getdata = async () => {
+    try {
+      const storedData = localStorage.getItem("userData");
+      const userData = JSON.parse(storedData);
+
+      const name = userData.data.uid;
+      console.log(name);
+
+      const res = await api.post(`/api/v1/user/ashadetail`, { name: name });
+      setuserdetails(res.data);
+      console.log(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEdit = () => {
+    setEditing(true);
+  };
+
+  const handleCancel = () => {
+    setEditing(false);
+  };
+
+  const handleSubmit = async (values) => {
+    try {
+      const storedData = localStorage.getItem("userData");
+      const userData = JSON.parse(storedData);
+
+      const name = userData.data.uid;
+      console.log(name);
+      const res = await api.post(
+        `/api/v1/user/ashaprofileupdate`,
+        { name: name },
+        values
+      );
+      // Handle success, show message or perform any additional actions
+      message.success("Profile updated successfully");
+      setEditing(false);
+    } catch (error) {
+      // Handle error, show error message or perform any additional actions
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getdata();
+  }, []);
+  const router = useRouter();
+  return (
+    <div className="container-profile">
+      <h1 className="heading-profile">Admin's Profile</h1>
+      <div className="circle-profile" />
+      <div className="rectangle-profile">
+        {editing ? (
+          <Form onFinish={handleSubmit} initialValues={userdetails}>
+            <Form.Item name="email" label="Email">
+              <Input />
+            </Form.Item>
+            <Form.Item name="phone" label="Phone">
+              <Input />
+            </Form.Item>
+            <Form.Item name="address" label="Address">
+              <Input />
+            </Form.Item>
+            <div className="line-profile" />
+            <Button type="primary" htmlType="submit">
+              Save
+            </Button>
+            <Button onClick={handleCancel}>Cancel</Button>
+          </Form>
+        ) : (
+          <>
+            <p>Name: {userdetails.name}</p>
+            <p>Age: {userdetails.age}</p>
+            <p>Gender: {userdetails.gender}</p>
+            <p>Email: {userdetails.email}</p>
+            <p>Phone: {userdetails.phone}</p>
+
+            <div className="line-profile" />
+            <Button className="smalltext-profile" onClick={handleEdit}>
+              Edit profile
+            </Button>
+          </>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default AdminProfile;
---
+++ clientnx/app/reportadmin/page.js
@@ -0,0 +1,88 @@
+"use client";
+import api from "@/api";
+import {
+  Form,
+  Input,
+  message,
+  Button,
+  Space,
+  DatePicker,
+  Select,
+  Upload,
+} from "antd";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useState } from "react";
+import Layout from "../layout";
+
+const { RangePicker } = DatePicker;
+const { TextArea } = Input;
+const normFile = (e) => {
+  if (Array.isArray(e)) {
+    return e;
+  }
+  return e?.fileList;
+};
+const ReportAdmin = () => {
+  const router = useRouter();
+  const [componentDisabled, setComponentDisabled] = useState(false);
+  const onFinish = async (values) => {
+    try {
+      values.age = Number(values.age);
+      values.phone = Number(values.phone);
+      values.ward_number = Number(values.ward_number);
+      const response = await api.post("/api/v1/user/addpatient", values);
+      console.log(response.data);
+      if (response.data.success) {
+        message.success("added successfully"); // Optional: Display the response from the backend
+      } // Perform any additional actions after successful submission
+    } catch (error) {
+      console.log(error);
+      message.error("something went wrong"); // Handle error case
+    }
+  };
+  return (
+    <Layout>
+      <h2 className="heading-report">Report Number</h2>
+      <div className="container-short">
+        <div className="report-container">
+          <Form
+            layout="horizontal"
+            disabled={componentDisabled}
+            onFinish={onFinish}
+            className="add-form"
+          >
+            <div className="content-report">
+              <p>
+                    <Form.Item label="Patient Name" name="name">
+                    <Input />
+                    </Form.Item>
+                    <Form.Item label="Patient ID" name="patient_id">
+                    <Input />
+                    </Form.Item>
+                    <Form.Item label="Visit Date" name="visit_date">
+                    <DatePicker />
+                    </Form.Item>
+                    <Form.Item label="Bood Pressure" name="assignd_asha">
+                    <Input />
+                    </Form.Item>
+                    <Form.Item label="Sugar" >
+                    <Input />
+                    </Form.Item>
+                    <Form.Item label="Remarks">
+                    <Input />
+                    </Form.Item>
+              </p>
+            </div>
+            <Space wrap>
+              <Button type="primary" htmlType="submit" style={{ backgroundColor: "#1b6871", borderColor: "#1b6871" }}>
+                Submit
+              </Button>
+            </Space>
+          </Form>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+export default ReportAdmin;
